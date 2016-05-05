3c2605b2dfca48aed47495b70453032d2477ed72
Samuel Bowler <samuel.bowler@gmail.com>
2016-05-05 16:56:17 -0500

    added signup forms to page1

--- client/app/account/signupArtist/signupArtist.controller.js
+++ client/app/account/signupArtist/signupArtist.controller.js
@@ -10,8 +10,8 @@ class SignupArtistController {
     this.$scope.currentUser = this.Auth.getCurrentUser();
   	// TODO:Add name_last to Signup
   	// TODO: Add name_artist to User
-
-  	this.$scope.$neighborhoods = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+  	
+  	this.$scope.neighborhoods = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
     'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
     'WY').split(' ').map(function(neighborhood) {
         return {abbrev: neighborhood};
