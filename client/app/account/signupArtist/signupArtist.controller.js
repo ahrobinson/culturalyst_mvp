d6f718cc84efc7a3625d9df575975536548378f0
Alon Robinson <ahrobins3@gmail.com>
2016-07-13 17:27:00 -0500

    fix styling issues

--- client/app/account/signupArtist/signupArtist.controller.js
+++ client/app/account/signupArtist/signupArtist.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-class SignupArtistController {
+class SignUpArtistController {
   constructor(Auth, $scope, $http, $mdToast, MediaList) {
     this.errors = {};
     this.Auth = Auth;
@@ -9,9 +9,9 @@ class SignupArtistController {
     this.MediaList = MediaList;
     this.$scope.mediaList = this.MediaList.getMediaList();
 
-
     //Getting CurrentUser
     this.$mdToast = $mdToast;
+
     //Getting MediaList Service
     this.$scope.currentUser = this.Auth.getCurrentUser();
     console.log(this.$scope.currentUser);
@@ -42,136 +42,133 @@ class SignupArtistController {
 
     //TODO: Allow Artist to have more than one medium and more than one genre per medium
 
-  	//TODO: Add in New Orleans Neighborhoods
-  	this.$scope.neighborhoods = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+    //TODO: Add in New Orleans Neighborhoods
+    this.$scope.neighborhoods = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
     'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
     'WY').split(' ').map(function(neighborhood) {
-        return {abbrev: neighborhood};
-      })
+        return { abbrev: neighborhood };
+      });
   }
 
-
-  loadGenres(medium){
-  	this.$scope.selectedMedium = medium.name;
-  	this.$scope.genreList = medium.genre;
-  	console.log(this.$scope.selectedMedium);
+  loadGenres(medium) {
+    this.$scope.selectedMedium = medium.name;
+    this.$scope.genreList = medium.genre;
+    console.log(this.$scope.selectedMedium);
   }
 
-  showBasicToast($mdToast){
-  	console.log('shit worked');
-  	 this.$mdToast.show(
+  showBasicToast($mdToast) {
+    console.log('shit worked');
+    this.$mdToast.show(
       this.$mdToast.simple()
-        .textContent('Basics Saved.')
-        .position('left')
-        .hideDelay(3000)
-        .parent('#saveBasics')
+          .textContent('Basics Saved.')
+          .position('left')
+          .hideDelay(3000)
+          .parent('#saveBasics')
     );
   }
 
-  showArtToast($mdToast){
-  	console.log('shit worked');
-  	 this.$mdToast.show(
+  showArtToast($mdToast) {
+    console.log('shit worked');
+    this.$mdToast.show(
       this.$mdToast.simple()
-        .textContent('Art Medium & Genre Saved.')
-        .position('left')
-        .hideDelay(3000)
-        .parent('#saveArt')
+          .textContent('Art Medium & Genre Saved.')
+          .position('left')
+          .hideDelay(3000)
+          .parent('#saveArt')
     );
   }
 
-  showPropertiesToast($mdToast){
-  	console.log('shit worked');
-  	 this.$mdToast.show(
+  showPropertiesToast($mdToast) {
+    console.log('shit worked');
+    this.$mdToast.show(
       this.$mdToast.simple()
-        .textContent('Properties Saved.')
-        .position('left')
-        .hideDelay(3000)
-        .parent('#saveProperties')
+          .textContent('Properties Saved.')
+          .position('left')
+          .hideDelay(3000)
+          .parent('#saveProperties')
     );
   }
 
-  showStoryToast($mdToast){
-  	console.log('shit worked');
-  	 this.$mdToast.show(
+  showStoryToast($mdToast) {
+    console.log('shit worked');
+    this.$mdToast.show(
       this.$mdToast.simple()
-        .textContent('Your story is saved.')
-        .position('left')
-        .hideDelay(3000)
-        .parent('#saveStory')
+          .textContent('Your story is saved.')
+          .position('left')
+          .hideDelay(3000)
+          .parent('#saveStory')
     );
   }
 
-  changeColor(){
+  changeColor() {
 
   }
-
 
   registerBasics() {
-  	let context = this;
+    let context = this;
 
-  	this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistBasics', {
-  		dob: context.$scope.dob,
-  		neighborhood:context.$scope.neighborhood,
-  		hometown: context.$scope.hometown,
-  		hometown_state: context.$scope.hometown_state,
-  	}).then(function(){
-  		// TODO: ADD animation here
-  		context.showBasicToast();
-  	})
+    this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistBasics', {
+      dob: context.$scope.dob,
+      neighborhood:context.$scope.neighborhood,
+      hometown: context.$scope.hometown,
+      hometown_state: context.$scope.hometown_state,
+    }).then(function () {
+      // TODO: ADD animation here
+      context.showBasicToast();
+    });
   }
 
-  registerArt(){
-  	let context = this;
-  	this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistArt', {
-  		medium: context.$scope.medium,
-  		genre: context.$scope.genre,
-  		experience: context.$scope.experience,
-  	}).then(function(){
-  		// TODO: ADD animation here
-  		context.showArtToast();
-  	})
+  registerArt() {
+    let context = this;
+    this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistArt', {
+      medium: context.$scope.medium,
+      genre: context.$scope.genre,
+      experience: context.$scope.experience,
+    }).then(function () {
+      // TODO: ADD animation here
+      context.showArtToast();
+    });
   }
 
-  registerProperties(){
-  	let context = this;
-  	this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistProperties', {
-  		website: context.$scope.website,
-  		instagram: context.$scope.instagram,
-  		twitter: context.$scope.twitter,
-  		store:context.$scope.store,
-  		soundcloud: context.$scope.soundcloud,
-  		behance: context.$scope.behance,
-  		bandcamp: context.$scope.bandcamp,
-  		spotify:context.$scope.spotify,
-  	}).then(function(){
-  		// TODO: ADD animation here
-  		context.showPropertiesToast();
-  	})
+  registerProperties() {
+    let context = this;
+    this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistProperties', {
+      website: context.$scope.website,
+      instagram: context.$scope.instagram,
+      twitter: context.$scope.twitter,
+      store:context.$scope.store,
+      soundcloud: context.$scope.soundcloud,
+      behance: context.$scope.behance,
+      bandcamp: context.$scope.bandcamp,
+      spotify:context.$scope.spotify,
+    }).then(function () {
+      // TODO: ADD animation here
+      context.showPropertiesToast();
+    });
   }
 
-  registerStory(){
-  	let context = this;
-  	this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistStory', {
-  		story: context.$scope.story
-  	}).then(function(){
-  		// TODO: ADD animation here
-  		context.showStoryToast();
-  	})
+  registerStory() {
+    let context = this;
+    this.$http.put('/api/users/' + this.$scope.currentUser._id + '/updateArtistStory', {
+      story: context.$scope.story
+    }).then(function () {
+      // TODO: ADD animation here
+      context.showStoryToast();
+    });
   }
 
-  launchProfile(){
-    console.log('launch profile')
+  launchProfile() {
+    console.log('launch profile');
     let context = this;
     this.$http.put('/api/users/' + this.$scope.currentUser._id + '/launchArtistProfile', {
       profile_pic: context.$scope.profile_pic,
       cover_pic: context.$scope.cover_pic,
       background_pic: context.$scope.background_pic,
-    }).then(function(){
+    }).then(function () {
       // TODO: ADD animation here
-    })
+    });
   }
 }
-
 
 angular.module('mvpApp')
   .controller('SignupArtistController', SignupArtistController);
